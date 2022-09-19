bfe4edb5c572aca1182c904a4dde5b8a7927a837
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-09-19 10:28:31 -0300

    renamed main.js

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+        width: 800,
+        height: 600,
     })
-  
-    win.loadFile('index.html')
-  }
 
-  app.whenReady().then(() => {
+    win.loadFile('index.html')
+
+}
+
+app.whenReady().then(() => {
     createWindow()
-  })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
